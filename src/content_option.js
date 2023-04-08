554d43afbb259d1c8ce406176e6ad57f52709432
SachinHanuvel <sachin@hanuvel.com>
2023-04-08 16:12:26 +0530

    React portfolio using hooks

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "SACHIN";
+const meta = {
+    title: "Sachin Pattar",
+    description: "I’m Sachin Pattar  Full stack devloper,currently working in Bengaluru",
+};
+
+const introdata = {
+    title: "I’m Sachin Pattar",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I Am Full Stack Java Developer",
+    },
+    description: "A skilled Full stack Java Developer with expertise in developing and maintaining end-to-end web applications using Java technologies.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "Proficient Full stack Java Developer with 1+ years of experience in designing and implementing end-to-end web applications using Java technologies and frameworks such as Spring Boot,React and Angular. Collaborative team player with a track record of delivering high-quality software products on time and within budget, seeking to leverage my skills and experience in a challenging Full stack Java Developer role",
+};
+const worktimeline = [{
+    //     jobtitle: "Designer of week",
+    //     where: "YAdfi",
+    //     date: "2020",
+    // },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "Jamalya",
+    //     date: "2019",
+    // },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "ALquds",
+    //     date: "2019",
+    },
+];
+// 
+const skills = [{
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "SpringBoot",
+        value: 85,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Angular",
+        value: 60,
+    },
+    {
+        name: "MySql",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Java Developer",
+        description: "Experienced Java developer proficient in designing and implementing scalable and maintainable software solutions using Java and related technologies.",
+    },
+    {
+        title: "Web Application",
+        description: "A skilled web application developer experienced in designing and developing efficient and user-friendly web applications utilizing a variety of programming languages and technologies..",
+    },
+    // {
+    //     title: "Wordpress Design",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+];
+
+const dataportfolio = [{
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/800/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sachinpattar928@gmail.com",
+    YOUR_FONE: "+91-9886872578",
+    // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    // YOUR_SERVICE_ID: "service_id",
+    // YOUR_TEMPLATE_ID: "template_id",
+    // YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    // github: "https://github.com",
+    // facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
